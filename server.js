3e03a98b04e9c1b2983d14df1a85e7381bb23e74
alizain721 <alizain721@gmail.com>
2018-09-06 13:01:35 +0500

    Port: Set port value for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // Folder to server custom js files
 app.use('/public', express.static(path.join(__dirname, '/public')));
 //Server setup
-app.listen(3000,function() {
+app.listen(process.env.PORT || 3000,function() {
   console.log('listening on 3000')
 });
 app.get('/chat',function(req,res){
