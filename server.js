5ed24312e932589c24252cb40e462fee6fee364c
alizain721 <alizain721@gmail.com>
2018-09-06 12:57:19 +0500

    Video:Implemet video and chat

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-import {Express} from "express"
-
-const app = Express();
-app.listen(3000, =>() {
+const express = require('express');
+var path = require('path')
+const app = express();
+// Folder to server custom js files
+app.use('/public', express.static(path.join(__dirname, '/public')));
+//Server setup
+app.listen(3000,function() {
   console.log('listening on 3000')
+});
+app.get('/chat',function(req,res){
+  res.sendFile(__dirname + '/index.html')
 })
+
